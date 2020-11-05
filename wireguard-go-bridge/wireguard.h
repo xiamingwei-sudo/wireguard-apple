3d92e24f8ceafd9305c51410d7f486a6d1837727
Jason A. Donenfeld <Jason@zx2c4.com>
2020-11-05 17:14:16 +0100

    Cherry pick cda99bf45c3cb95ca56204549689a0ae91ff4813 from jd/loggerCtx

--- wireguard-go-bridge/wireguard.h
+++ wireguard-go-bridge/wireguard.h
@@ -12,7 +12,7 @@
 
 typedef void(*logger_fn_t)(int level, const char *msg);
 extern void wgEnableRoaming(bool enabled);
-extern void wgSetLogger(logger_fn_t logger_fn);
+extern void wgSetLogger(void *context, logger_fn_t logger_fn);
 extern int wgTurnOn(const char *settings, int32_t tun_fd);
 extern void wgTurnOff(int handle);
 extern int64_t wgSetConfig(int handle, const char *settings);
