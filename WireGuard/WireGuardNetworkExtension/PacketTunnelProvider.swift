329a5c2880c9fcc5d5e2a2a0007683dff0ea421f
Andrej Mihajlov <and@mullvad.net>
2020-11-09 13:24:50 +0100

    Implement handleAppMessage

--- WireGuard/WireGuardNetworkExtension/PacketTunnelProvider.swift
+++ WireGuard/WireGuardNetworkExtension/PacketTunnelProvider.swift
@@ -5,6 +5,19 @@ import Foundation
 import NetworkExtension
 import WireGuardKit
 
-class PacketTunnelProvider {
-
+class PacketTunnelProvider: WireGuardPacketTunnelProvider {
+    override func handleAppMessage(_ messageData: Data, completionHandler: ((Data?) -> Void)? = nil) {
+        if messageData.count == 1 && messageData[0] == 0 {
+            getWireGuardConfiguration { settings in
+                if let settings = settings {
+                    let data = settings.data(using: .utf8)!
+                    completionHandler?(data)
+                } else {
+                    completionHandler?(nil)
+                }
+            }
+        } else {
+            completionHandler?(nil)
+        }
+    }
 }
