d871cb7ecf89e67301528ce5aa04239bf7f79db5
Andrej Mihajlov <and@mullvad.net>
2020-11-09 10:52:06 +0100

    Integrate WireGuardKit

---
+++ WireGuard/WireGuardNetworkExtension/PacketTunnelProvider.swift
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+// Copyright © 2018-2019 WireGuard LLC. All Rights Reserved.
+
+import Foundation
+import NetworkExtension
+import WireGuardKit
+
+class PacketTunnelProvider {
+
+}
