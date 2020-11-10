dbec40c88f0d35ede1afaa32b835c5931672934f
Andrej Mihajlov <and@mullvad.net>
2020-11-10 13:36:56 +0100

    Rewrite Curve25519 without using `withUnsafeMutableUInt8Bytes`

--- WireGuard/WireGuard/Crypto/Curve25519.swift
+++ WireGuard/WireGuard/Crypto/Curve25519.swift
@@ -9,24 +9,17 @@ struct Curve25519 {
     static let keyLength: Int = 32
 
     static func generatePrivateKey() -> Data {
-        var privateKey = Data(repeating: 0, count: TunnelConfiguration.keyLength)
-        privateKey.withUnsafeMutableUInt8Bytes { bytes in
-            curve25519_generate_private_key(bytes)
-        }
-        assert(privateKey.count == TunnelConfiguration.keyLength)
-        return privateKey
+        var privateKey = [UInt8](repeating: 0, count: TunnelConfiguration.keyLength)
+        curve25519_generate_private_key(&privateKey)
+        return Data(privateKey)
     }
 
     static func generatePublicKey(fromPrivateKey privateKey: Data) -> Data {
         assert(privateKey.count == TunnelConfiguration.keyLength)
-        var publicKey = Data(repeating: 0, count: TunnelConfiguration.keyLength)
-        privateKey.withUnsafeUInt8Bytes { privateKeyBytes in
-            publicKey.withUnsafeMutableUInt8Bytes { bytes in
-                curve25519_derive_public_key(bytes, privateKeyBytes)
-            }
-        }
-        assert(publicKey.count == TunnelConfiguration.keyLength)
-        return publicKey
+        var publicKeyBytes = [UInt8](repeating: 0, count: TunnelConfiguration.keyLength)
+        var privateKeyBytes = [UInt8](privateKey)
+        curve25519_derive_public_key(&publicKeyBytes, &privateKeyBytes)
+        return Data(publicKeyBytes)
     }
 }
 
